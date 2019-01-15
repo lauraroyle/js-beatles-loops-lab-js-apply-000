08e96180be8dd2a91e9706d21345067727c96a17
Laura Royle <lauraamie.royle@gmail.com>
2019-01-15 08:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musicianString;
+  var musicianInstrument=[];
+    for (var i = 0; i < musicians.length; i++) {
+      musicianString = musicians[i] + ' plays ' + instruments[i];
+      musicianInstrument[i] = musicianString;
+      {
+        return musicianInstrument;
+      }
+    }
+}
