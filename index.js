fd3953464de4a8a33bda70290b7bf89971dda53f
Laura Royle <lauraamie.royle@gmail.com>
2019-01-15 09:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for(var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + 'plays' + instruments[i]);
+    array.push(musicians[i] + ' plays ' + instruments[i]);
 }
   return array;
 }
+
